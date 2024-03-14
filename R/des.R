4b2f7208c74e65f0cff5a626a268a65a00dd26cc
PallabiniDash <Pallabinidash9@gmail.com>
2024-03-14 17:50:08 -0400

    Add files via upload

---
+++ R/des.R
@@ -0,0 +1,17 @@
+#'creates histogram,boxplot,numeric summary
+#' @export
+#' @param x numeric variable
+
+des <- function(x) {
+  # 1 row and 2 column
+  par(mfrow=c(1,2))
+  #histogram
+  hist(x,col=rainbow(30))
+  #boxplot
+  boxplot(x,col='green')
+  par(mfrow=c(1,1))
+ #numeric summary
+  data.frame(min=min(x),
+             max=max(x),sd=sd(x),
+             mean=mean(x),median=median(x))
+}
